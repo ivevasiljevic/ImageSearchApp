4859c65fe86ae513ba77f9417cec75f1b782f96e
ivevasiljevic <ive@profico.hr>
2021-05-14 13:12:26 +0200

    add item and fragment layouts and model class

---
+++ app/src/main/java/personal/ive/imagesearchapp/GalleryFragment.kt
@@ -0,0 +1,12 @@
+package personal.ive.imagesearchapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
