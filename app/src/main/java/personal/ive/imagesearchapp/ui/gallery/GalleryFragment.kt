6a82ff8054b6efa8653adad42177eea8de98d0a9
ivevasiljevic <ive@profico.hr>
2021-05-17 22:34:13 +0200

    add paging data adapter

--- app/src/main/java/personal/ive/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/personal/ive/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,22 +1,46 @@
 package personal.ive.imagesearchapp.ui.gallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import personal.ive.imagesearchapp.R
+import personal.ive.imagesearchapp.databinding.FragmentGalleryBinding
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel: GalleryViewModel by viewModels()
+    private var _binding: FragmentGalleryBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
-            //something
+        _binding = FragmentGalleryBinding.bind(view)
+
+        val unsplashPhotoAdapter = UnsplashPhotoAdapter()
+
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = unsplashPhotoAdapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+
+            Log.d("TAG", "onViewCreated: $it")
+            unsplashPhotoAdapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/personal/ive/imagesearchapp/ui/gallery/UnsplashPhotoAdapter.kt
@@ -0,0 +1,58 @@
+package personal.ive.imagesearchapp.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import personal.ive.imagesearchapp.R
+import personal.ive.imagesearchapp.data.UnsplashPhoto
+import personal.ive.imagesearchapp.databinding.ItemUnsplashPhotoBinding
+
+/**
+ * Created by ivasil on 5/17/2021
+ */
+
+class UnsplashPhotoAdapter : PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.PhotoViewHolder>(PhotoComparator) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding = ItemUnsplashPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if(currentItem != null) {
+            holder bindTo currentItem
+        }
+    }
+
+    inner class PhotoViewHolder(private val binding: ItemUnsplashPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        infix fun bindTo(photo: UnsplashPhoto) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+                    .into(imageView)
+
+                userName.text = photo.user.username
+            }
+        }
+    }
+
+    object PhotoComparator : DiffUtil.ItemCallback<UnsplashPhoto>() {
+
+        override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean =
+            oldItem == newItem
+
+    }
+}
